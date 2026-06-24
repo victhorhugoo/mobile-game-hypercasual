9aaa4d5464cc1ca7f24358ae4a2240bd706a0e41
victhorhugoo <163677031+victhorhugoo@users.noreply.github.com>
2026-06-24 19:04:57 -0300

    feat: add start and finish game screens

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,17 +4,58 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 1f;
+    [Header("Lerp")]
+    public Transform target;
+    public float lerpSpeed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 1f;
+    public string tagCheckEnemy = "Enemy";
+    public string tagCheckEndLine = "EndLine";
+
+    public GameObject endScreen;
+    public GameObject startScreen;
+
+    private bool _canRun;
+    private Vector3 _pos;
 
     // Update is called once per frame
     void Update()
     {
+        if (!_canRun) return;
+
+        _pos = target.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
+
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
         transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == tagCheckEnemy)
+        {
+            EndGame();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == tagCheckEndLine)
+        {
+            EndGame();
+        }
+    }
+
+    private void EndGame()
+    {
+        _canRun = false;
+        endScreen.SetActive(true);
+    }
+
+    public void StartToRun()
+    {
+        _canRun = true;
+        startScreen.SetActive(false);
     }
 }
