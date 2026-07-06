80e641dc2222ea87f14034a48c0fff175a66d471
victhorhugoo <163677031+victhorhugoo@users.noreply.github.com>
2026-07-06 00:17:59 -0300

    feat: add ui animations screen

---
+++ Assets/Scripts/Screen/ScreenBase.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+using DG.Tweening;
+
+namespace Screens
+{
+    public enum ScreenType
+    {
+        Panel,
+        Info_Panel,
+        Shop
+    }
+
+    public class ScreenBase : MonoBehaviour
+    {
+        public ScreenType screenType;
+
+        public List<Transform> listOfObjects;
+
+        public bool startHided = false;
+
+        [Header("Animation")]
+        public float delayBetweenObjects = .05f;
+        public float animationDuration = .3f;
+
+        private void Start()
+        {
+            if (startHided)
+            {
+                HideObjects();
+            }
+        }
+
+        [ Button ]
+        protected virtual void Show()
+        {
+            Debug.Log("Show");
+            ShowObjects();
+        }
+        
+        [Button]
+        protected virtual void Hide()
+        {
+            Debug.Log("Hide");
+            HideObjects();
+        }
+
+        private void ShowObjects()
+        {
+            for(int i = 0; i < listOfObjects.Count; i++)
+            {
+                var obj = listOfObjects[i];
+                obj.gameObject.SetActive(true);
+                obj.DOScale(0, animationDuration).From().SetDelay(i * delayBetweenObjects);
+
+            }
+        }
+
+        private void HideObjects()
+        {
+            listOfObjects.ForEach(i => i.gameObject.SetActive(false));
+        }
+        private void ForceShowObjects()
+        {
+            listOfObjects.ForEach(i => i.gameObject.SetActive(true));
+        }
+    }
+}
